465ba51082ef8ab76947478e3438058c640daeff
Rulo77 <ragomez1234567@gmail.com>
2025-10-31 16:32:15 -0600

    feat: implementacion de productos en memoria

--- packages/products-service/src/products/entities/product.entity.ts
+++ packages/products-service/src/products/entities/product.entity.ts
@@ -1,1 +1,24 @@
-export class Product {}
+export class Product {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(
+    id: string,
+    name: string,
+    price: number,
+    quantity: number,
+    createdAt?: Date,
+    updatedAt?: Date,
+  ) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+    this.createdAt = createdAt || new Date();
+    this.updatedAt = updatedAt || new Date();
+  }
+}
--- packages/products-service/src/products/products.service.ts
+++ packages/products-service/src/products/products.service.ts
@@ -1,15 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  private items = [
+    {
+      id: 'adafag_1',
+      name: 'laptop',
+      price: 45000,
+      quantity: 10,
+      createdAt: '2025-10-15 14:00:00',
+      updatedAt: '2025-10-15 14:00:00',
+    },
+    {
+      id: 'adafag_2',
+      name: 'television',
+      price: 5000,
+      quantity: 5,
+      createdAt: '2025-10-15 14:00:00',
+      updatedAt: '2025-10-15 14:00:00',
+    },
+  ];
+  private product: Product[] = Array.from(
+    this.items,
+    (item) =>
+      new Product(
+        item.id,
+        item.name,
+        item.price,
+        item.quantity,
+        new Date(item.createdAt),
+        new Date(item.updatedAt),
+      ),
+  );
+
   create(createProductDto: CreateProductDto) {
     return `This action adds a new product ${JSON.stringify(createProductDto)}`;
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.product;
   }
 
   findOne(id: number) {
